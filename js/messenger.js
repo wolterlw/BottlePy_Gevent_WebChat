f906218fdc732cabab40f8d950006a9b47b9bfd8
eugene <eugene.rublenko@gmail.com>
2016-05-07 12:46:04 +0300

    initial routes config in messenger module

--- js/messenger.js
+++ js/messenger.js
@@ -1,1 +1,23 @@
-var msngr = angular.module("messenger", ["ngRoute"]);
+var msngr = angular.module('messenger', ['ngRoute']);
+
+msngr.config(function ($routeProvider) {
+   	$routeProvider
+		.when('/log-in', {
+			templateUrl: 'log-in.tmpl.html',
+			controller: function() {},
+			controllerAs: 'a'
+		})
+		.when('/messages', {
+			templateUrl: 'messages.tmpl.html',
+			controller: function() {},
+			controllerAs: 'b'
+		})
+		.when('/dialog', {
+			templateUrl: 'dialog.tmpl.html',
+			controller: function() {},
+			controllerAs: 'c'
+		})
+		.otherwise({
+			redirectTo: '/log-in'
+	    });
+});
