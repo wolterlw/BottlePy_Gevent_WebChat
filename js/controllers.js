b13dc1a783f2dfce5bb02469933235294cbb5de6
eugene <eugene.rublenko@gmail.com>
2016-05-07 15:38:43 +0300

    add feature that switches sign-in and sign-up blocks in log-in.html (changes in loginController)

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,17 @@
 var msngrControllers = angular.module('cControllers', []);
 
 msngrControllers.controller('LoginController', function () {
+
+	//have no idea where it supposed to be
+    $("#menu-toggle").fadeOut(0);
+
 	this.credentials = {};
 	this.submit = function() {
 		console.log(this.credentials);
+		//use service for logging 
+	}
+	this.toggleLogin = function() {
+        $(".form-signin").remove();
+        $(".form-signup").fadeIn();
 	}
 });
