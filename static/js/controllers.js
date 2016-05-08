655f627c3dd2fd67cb59989cd80d5b7448b569a6
eugene <eugene.rublenko@gmail.com>
2016-05-08 19:26:36 +0300

    create LoginService for signing in and signing up

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -4,24 +4,17 @@ msngrControllers.controller('LoginController', function () {
 	//have no idea where it supposed to be
     $("#menu-toggle").fadeOut(0);
 });
-msngrControllers.controller('SignInController', function($http) {
+msngrControllers.controller('SignInController', ['LoginService', function(LoginService) {
 	this.credentials = {};
 	this.submit = function() {
-		console.log(this.credentials);
-		//use service for logging
-		$http.post('/login', this.credentials)
-            .success(function (data) {
-               console.log("success: ", data);
-            })
-            .error(function (data) {
-               console.log("error: ", data);
-            });
+		console.log("SignInController: this.credentials=", this.credentials);
+		LoginService.signIn(this.credentials);
 	}
 	this.toggleLogin = function() {
         $(".form-signin").remove();
         $(".form-signup").fadeIn();
 	}
-});
+}]);
 msngrControllers.controller('SignUpController', function() {
 	this.newUser = {};
 	this.submit = function() {
--- static/js/services.js
+++ static/js/services.js
@@ -1,5 +1,19 @@
 var msngrServices = angular.module('cServices', []);
 
-msngrServices.factory('signInService', ['$http', function($http) {
+msngrServices.factory('LoginService', ['$http', function($http) {
+	var obj = {};
+	obj.signIn = function(credentials) {
+		$http.post('/login', credentials)
+            .success(function (data) {
+				console.log("LoginService signIn: data=", data);
+				//save id to auth service
+				//redirect
+            })
+            .error(function (data) {
+            	console.log("LoginService signIn: data=", data);
+            	//show error message and other shit for damn user
+            });
+	}
 
+	return obj;
 }]);
